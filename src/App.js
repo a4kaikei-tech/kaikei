311e871121efd7a9a7132d5120d45d98286994c6
a4kaikei-tech <a4kaikei@gmail.com>
2026-04-04 00:39:22 +0900

    Update placeholder text for company name input

--- src/App.js
+++ src/App.js
@@ -170,7 +170,7 @@ function AuthPage({ page, setPage, showToast }) {
                 <label style={{ fontSize: 12, color: "#94A3B8", marginBottom: 6, display: "block" }}>名前</label>
                 <input
                   style={inputBase}
-                  placeholder="株式会社サンプル"
+                  placeholder="熊野A4太郎"
                   value={companyName}
                   onChange={e => setCompanyName(e.target.value)}
                 />
